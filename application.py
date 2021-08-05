cbf4769016b2514cb3720f327f46490fcbcbae3f
Aya Inoue <ainoue@hpe.com>
2021-08-05 16:29:24 +0900

    8/5

--- application.py
+++ application.py
@@ -1,26 +1,30 @@
 import json
-
-# def helloworld():
-#     name = input('input your name: ')
-#     message = 'Hello ' + name + ' !'
-#     print(message)
-
-# helloworld()
-
-def main():
-    f = open('fortuneresult.json','r',encoding='utf-8')
-
-    jsn = json.load(f)
+# import Spotify
+import fortune_json
 
 # with open('fortuneresult.json', encoding='utf-8') as f:
 #     jsn = json.load(f)
 
     # for a in jsn['牡羊座']:
-    print(jsn.get("牡羊座").get("total"))
-    total = jsn.get("牡羊座").get("total")
-    # if total > 4:
+    # print(jsn.get("牡羊座").get("total"))
+    # total = jsn.get("牡羊座").get("total")
+    # money = jsn.get("牡羊座").get("money")
+    # print(jsn.get("牡羊座").get("money"))
 
-    # print("{}".format(json.dumps(jsn,indent=2)))
+max_val = 0
+element = ''
 
-if __name__=='__main__':
-    main()
+if fortune_json.point_dict['牡羊座']['total'] > max_val:
+    max_val = fortune_json.point_dict['牡羊座']['total']
+    element = 'valence'
+elif fortune_json.point_dict['牡羊座']['job'] > max_val:
+    max_val = fortune_json.point_dict['牡羊座']['job']
+    element = 'energy'
+elif fortune_json.point_dict['牡羊座']['money'] > max_val:
+    max_val = fortune_json.point_dict['牡羊座']['money']
+    element = 'danceability'
+else:
+    max_val = fortune_json.point_dict['牡羊座']['love']
+    element = 'acousticness'
+
+print(element)
