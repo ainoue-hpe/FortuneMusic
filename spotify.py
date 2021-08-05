de766951183ab2da67d2b26272f47b762bd285df
githubtsukamon874397 <githubnarumi.tsukamoto@hpe.com>
2021-08-05 13:56:29 +0900

    8/5

--- spotify.py
+++ spotify.py
@@ -20,93 +20,17 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="023480561f
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 songsjp = pd.read_csv("regional-jp-daily-latest.csv",header=1)
-# songsjp.head(200)
-# print (songsjp.head(200))
 
 songsgl = pd.read_csv("regional-global-daily-latest.csv",header=1)
-# songsjp.head(200)
-# print (songsjp.head(200))
 
 song_info = pd.DataFrame()
-# 楽曲数分の情報を取得
+
 for url in songsjp["URL"]: 
     n=spotify.audio_features(url)[0]
-    # df= pd.DataFrame.from_dict(n)
-    # df= pd.DataFrame.from_dict(spotify.audio_features(url)[0])
-    # print(n)
-    # print(df)
-    # print (spotify.audio_features(url))
-
  
-    
-# results = sp.search(q='japantop50', limit=20,type='playlist')
-# for idx, track in enumerate(results['artist']['name']):
-#     print(idx, track['artist']['name'])
+file = pd.read_csv('日本TOP200.csv')
+file_sort = file.sort_values('danceability')
+file_URL = file_sort.loc[:,'URL']
 
-#     song_info = songsjp.append(df)
-# song_info.head(10)
-# print(song_info.head(10))
-# songsjp = pd.DataFrame() # 空のdfを作成
-
-
-
-# for url in songsjp["URL"]: # jp_dailyのURL列を1つずつなめる
-#    a = pd.DataFrame.from_dict(spotify.audio_features(url)) # URLに該当する曲の特徴を抽出
-#    songsjp = songsjp.append(a) # 読み込んだ特徴を行へ追加
-
-# songsjp = songsjp.reset_index(drop=True)
-
-
-
-#songs=np.arange(1000).reshape((200,5))
-#np.set_printoptions(threshold=np.inf)
-
-# pd.set_option("display.max_rows",None)
-
-#for track in songs['tracks']:
-#  print(track)
-
-# songsgl = pd.read_csv("regional-global-daily-latest.csv",header=1)
-# songsgl.head(200)
-# print (songsgl.head(200))
-
-# songs_info_jp = pd.DataFrame()
-# for url in songsjp["URL"]: 
-#   df = pd.DataFrame.from_dict(spotify.audio_features(url))
-#   songs_info_jp= songs_info_jp.append(df)
-# songs_info_jp.head(10)
-
-# songs_info_jp = songs_info_jp.reset_index(drop = True)
-# songs_info_jp.head(10)
-
-
-# df = pd.DataFrame
-# df.head()
-
-
-
-# def track_features(track_ids, limit = 20):
-#     return pd.DataFrame(sp.audio_features(track_ids))
-
-# print(track_features(track_info()['track_id']).drop(['track_href', 'analysis_url', 'id', 'uri'], axis=1))
-
-
-# newlist_jp= sorted("日本TOP200")
-# print(org_list)
-# print(newlist_jp)
-
-
-#     pprint.pprint(sorted(l_2d, key=lambda x: x[5]), width=200)
-
-# with open('日本TOP200.csv') as csvfile:
-#     print(csvfile.sort_values('danceability'))
-with open("日本TOP200.csv", "r",encoding="utf-8_sig") as f:
-    reader = csv.reader(f)
-    # f.write("日本TOP200.CSV")
-    # operator.itemgetter(0)で一列目をソート
-    file = (sorted (reader, key=operator.itemgetter(6)))
-with open("sortjp.csv",'a', encoding="utf-8_sig") as f:
-    writer = csv.writer(f)
-    writer.writerow(file)
-
+print(file_URL)
     
