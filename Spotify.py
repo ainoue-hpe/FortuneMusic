7ca88ea25b185011973b426aee5a5a2360fe07bb
Aya Inoue <ainoue@hpe.com>
2021-08-06 10:43:29 +0900

    8/6

---
+++ Spotify.py
@@ -0,0 +1,49 @@
+import csv, operator
+import pandas as pd
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import sys
+import numpy as np
+import random
+import application
+
+lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+
+client_id = '023480561f7749a182e794a5ec355145'
+client_secret = 'd5f92f42727842bbaa8d211abbee5cc2'
+client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials(client_id, client_secret)
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="023480561f7749a182e794a5ec355145",
+                                                           client_secret="d5f92f42727842bbaa8d211abbee5cc2"))
+
+
+
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+songsjp = pd.read_csv("regional-jp-daily-latest.csv",header=1)
+
+songsgl = pd.read_csv("regional-global-daily-latest.csv",header=1)
+
+song_info = pd.DataFrame()
+
+for url in songsjp["URL"]: 
+    n=spotify.audio_features(url)[0]
+ 
+
+number=random.randint(0,1)
+print(number)
+if number==0:
+
+    word=application.element
+    file_jp = pd.read_csv('日本TOP200.csv')
+    file_sort_jp = file_jp.sort_values('%s' % word,ascending=False)
+    file_URL_jp = file_sort_jp.loc[:,'URL']
+    print('\n'.join(file_URL_jp.to_list()))
+
+elif number==1:
+    word=application.element
+    file_gl = pd.read_csv('グローバルTOP200.csv')
+    file_sort_gl = file_gl.sort_values('%s' % word,ascending=False)
+    file_URL_gl = file_sort_gl.loc[:,'URL']
+    print('\n'.join(file_URL_gl.to_list()))
--- application.py
+++ application.py
@@ -1,6 +1,31 @@
-def helloworld():
-    name = input('input your name: ')
-    message = 'Hello ' + name + ' !'
-    print(message)
+import json
+# import Spotify
+import fortune_json
 
-helloworld()
+# with open('fortuneresult.json', encoding='utf-8') as f:
+#     jsn = json.load(f)
+
+    # for a in jsn['牡羊座']:
+    # print(jsn.get("牡羊座").get("total"))
+    # total = jsn.get("牡羊座").get("total")
+    # money = jsn.get("牡羊座").get("money")
+    # print(jsn.get("牡羊座").get("money"))
+
+max_val = 0
+element = ''
+sign = '山羊座'
+
+if fortune_json.point_dict[sign]['total'] > max_val:
+    max_val = fortune_json.point_dict[sign]['total']
+    element = 'valence'
+    if fortune_json.point_dict[sign]['job'] > max_val:
+        max_val = fortune_json.point_dict[sign]['job']
+        element = 'energy'
+        if fortune_json.point_dict[sign]['money'] > max_val:
+            max_val = fortune_json.point_dict[sign]['money']
+            element = 'danceability'
+            if fortune_json.point_dict[sign]['love'] > max_val:
+                max_val = fortune_json.point_dict[sign]['love']
+                element = 'acousticness'
+
+print(element)
